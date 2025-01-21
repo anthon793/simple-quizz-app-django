1c88e57ddad54f93e40c90beea33bb84b6030977
amehanthony <amehanthonyowoi@gmail.com>
2025-01-21 11:43:58 -1200

    Updated views and url files

--- quizzapp/urls.py
+++ quizzapp/urls.py
@@ -1,13 +1,26 @@
-from django.urls import path
+# from django.urls import path
 
-from . import views
+# from . import views
+
+# urlpatterns = [
+#     path("", views.index, name="index"),
+#     path('game', views.game, name='game'),
+#     # path('end/', views.end, name='end'),
+#     # path('highscores', views.highscores, name='highscores'),
+#     # path('register', views.registerPage, name = "register"),
+#     # path('login', views.loginPage, name = "login"),
+#     # path('logout', views.logoutUser, name = "logout")
+# ]
+
+from django.urls import path
+from .views import IndexView, GameView, EndView, HighscoresView, RegisterView, LoginView, LogoutView
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path('game', views.game, name='game'),
-    path('end/', views.end, name='end'),
-    path('highscores', views.highscores, name='highscores'),
-    path('register', views.registerPage, name = "register"),
-    path('login', views.loginPage, name = "login"),
-    path('logout', views.logoutUser, name = "logout")
+    path('', IndexView.as_view(), name='index'),
+    path('game', GameView.as_view(), name='game'),
+    path('end', EndView.as_view(), name='end'),
+    path('highscores', HighscoresView.as_view(), name='highscores'),
+    path('register', RegisterView.as_view(), name='register'),
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
--- quizzapp/views.py
+++ quizzapp/views.py
@@ -1,69 +1,152 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 from .forms import CreateUserForm
+from .game import QuizGame
 
-from django.http import HttpResponse
+class GameView(LoginRequiredMixin, View):
+    login_url = 'login'
 
-@login_required(login_url='login')
-def index(request):
-    return render(request, 'index.html')
+    def get(self, request):
+        game = QuizGame()
+        game.fetch_questions()
+        context = {'questions': game.questions}
+        return render(request, 'game.html', context)
+class BaseView(View):
+    def get(self, request):
+        return render(request, self.template_name)
 
-@login_required(login_url='login')
-def game(request):
-    return render(request, 'game.html')
+class IndexView(LoginRequiredMixin, BaseView):
+    login_url = 'login'
+    template_name = 'index.html'
 
-@login_required(login_url='login')
-def end(request):
-    return render(request, 'end.html')
+class GameView(LoginRequiredMixin, BaseView):
+    login_url = 'login'
+    template_name = 'game.html'
 
-@login_required(login_url='login')
-def highscores(request):
-    return render(request, 'highscores.html')
+class EndView(LoginRequiredMixin, BaseView):
+    login_url = 'login'
+    template_name = 'end.html'
 
-def registerPage(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    else:
+class HighscoresView(LoginRequiredMixin, BaseView):
+    login_url = 'login'
+    template_name = 'highscores.html'
+
+class RegisterView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('index')
         form = CreateUserForm()
+        return render(request, 'register.html', {'form': form})
 
-        if request.method == 'POST':
-            form = CreateUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-                user = form.cleaned_data.get('username')
-                messages.success(request, 'Account was created for ' + user)
+    def post(self, request):
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+        return render(request, 'register.html', {'form': form})
 
-                return redirect('login')
-        context = {'form':form}
-        return render(request, 'register.html', context)
+class LoginView(View):
+    def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('index')
+        return render(request, 'login.html')
 
-def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('index')
-    else: 
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        messages.info(request, 'Username or password is incorrect')
+        return render(request, 'login.html')
 
-            user = authenticate(request, username = username, password=password)
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('login')
+    
 
-            if user is not None:
-                login(request, user)
-                return redirect('index')
-            else:
-                messages.info(request, 'Username or password is incorrect')
 
-        context = {}
-        return render(request, 'login.html', context)
 
-def logoutUser(request):
-    logout(request)
-    return redirect('login')
+
+
+
+# from django.shortcuts import render,redirect
+# from django.contrib.auth.forms import UserCreationForm
+
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib import messages
+
+# from django.contrib.auth.decorators import login_required
+
+# # Create your views here.
+
+# from .forms import CreateUserForm
+
+# from django.http import HttpResponse
+
+# @login_required(login_url='login')
+# def index(request):
+#     return render(request, 'index.html')
+
+# @login_required(login_url='login')
+# def game(request):
+#     return render(request, 'game.html')
+
+# @login_required(login_url='login')
+# def end(request):
+#     return render(request, 'end.html')
+
+# @login_required(login_url='login')
+# def highscores(request):
+#     return render(request, 'highscores.html')
+
+# def registerPage(request):
+#     if request.user.is_authenticated:
+#         return redirect('index')
+#     else:
+#         form = CreateUserForm()
+
+#         if request.method == 'POST':
+#             form = CreateUserForm(request.POST)
+#             if form.is_valid():
+#                 form.save()
+#                 user = form.cleaned_data.get('username')
+#                 messages.success(request, 'Account was created for ' + user)
+
+#                 return redirect('login')
+#         context = {'form':form}
+#         return render(request, 'register.html', context)
+
+# def loginPage(request):
+#     if request.user.is_authenticated:
+#         return redirect('index')
+#     else: 
+#         if request.method == 'POST':
+#             username = request.POST.get('username')
+#             password = request.POST.get('password')
+
+#             user = authenticate(request, username = username, password=password)
+
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('index')
+#             else:
+#                 messages.info(request, 'Username or password is incorrect')
+
+#         context = {}
+#         return render(request, 'login.html', context)
+
+# def logoutUser(request):
+#     logout(request)
+#     return redirect('login')
+
+
